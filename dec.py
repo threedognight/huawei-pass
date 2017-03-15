6c69df6af68ba66792c07c015d014cbd586d46b4
Nick <nbobrovsky@gmail.com>
2017-03-15 18:25:26 +0300

    Refactoring power method

--- dec.py
+++ dec.py
@@ -7,9 +7,6 @@ p = bytearray(b'\xb8\x36\x3c\x9b\x77\xda\xed\x4b\x9a\xbb\x9f\x2f\x6d\xf5\xf1\xd5
 for j in range(len(n)):
     print(format(n[j], '02X'), end=' ')
 print()
-# "D:\Program Files (x86)\Python\Python36-32\python.exe" C:/Users/Loki/PycharmProjects/huawei/dec.py
-# 0x70 0x22 0x56 0x42 0x41 0x51 0x39 0x3c 0x52 0x3b 0x60 0x74 0x7d 0x58 0x52 0x4f 0x5e 0x4d 0x67 0x52 0x21 0x25 0x21 0x21
-# Process finished with exit code 0
 crypted_string = bytearray(n)
 for i in range(len(crypted_string)):
     if crypted_string[i] != 0x7e:
@@ -19,10 +16,10 @@ for i in range(len(crypted_string)):
 for j in range(len(crypted_string)):
     print(format(crypted_string[j], '02X'), end=' ')
 
-a1 = crypted_string[0]+crypted_string[1]*93+crypted_string[2]*93*93+crypted_string[3]*93*93*93+crypted_string[4]*93*93*93*93
-a2 = crypted_string[5]+crypted_string[6]*93+crypted_string[7]*93*93+crypted_string[8]*93*93*93+crypted_string[9]*93*93*93*93
-a3 = crypted_string[10]+crypted_string[11]*93+crypted_string[12]*93*93+crypted_string[13]*93*93*93+crypted_string[14]*93*93*93*93
-a4 = crypted_string[15]+crypted_string[16]*93+crypted_string[17]*93*93+crypted_string[18]*93*93*93+crypted_string[19]*93*93*93*93
+a1 = crypted_string[0]+crypted_string[1]*93+crypted_string[2]*93**2+crypted_string[3]*93**3+crypted_string[4]*93**4
+a2 = crypted_string[5]+crypted_string[6]*93+crypted_string[7]*93**2+crypted_string[8]*93**3+crypted_string[9]*93**4
+a3 = crypted_string[10]+crypted_string[11]*93+crypted_string[12]*93**2+crypted_string[13]*93**3+crypted_string[14]*93**4
+a4 = crypted_string[15]+crypted_string[16]*93+crypted_string[17]*93**2+crypted_string[18]*93**3+crypted_string[19]*93**4
 print('\n')
 print(binascii.hexlify(n))
 print(binascii.hexlify(crypted_string))
