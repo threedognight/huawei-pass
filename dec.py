382bc9e0678bae84ddd5ce82c510914e11d6ab83
Nick <nbobrovsky@gmail.com>
2017-03-15 20:46:44 +0300

    Refactor variables names

--- dec.py
+++ dec.py
@@ -2,10 +2,10 @@
 import binascii
 import struct
 
-# n = bytearray(b'p"VBAQ9<R;`t}XRO^MgR!%!!')
+# n = bytearray(b'initial_ECB"VBAQ9<R;`t}XRO^MgR!%!!')
 # n = bytearray(b'RT0}%[uVW16%Zb~-d5zONyx!')
 n = bytearray(b'+_`!X\'4l_$hPsGVEaT*:E"2!')
-p = bytearray(b'b8363c9b77daed4b9abb9f2f6df5f1d5cb64975d5d3bcee8827f2f42235f9229')
+initial_ECB = bytearray(b'b8363c9b77daed4b9abb9f2f6df5f1d5cb64975d5d3bcee8827f2f42235f9229')
 for j in range(len(n)):
     print(format(n[j], '02X'), end=' ')
 print()
@@ -33,10 +33,10 @@ print(hex(a1) + ' ' + hex(a2) + ' ' + hex(a3) + ' ' + hex(a4))
 print(binascii.hexlify(struct.pack("<L", a1)) + binascii.hexlify(struct.pack("<L", a2)) + binascii.hexlify(
     struct.pack("<L", a3)) + binascii.hexlify(struct.pack("<L", a4)))
 k = 20
-pass_ECB = bytearray(p)
+pass_ECB = bytearray(initial_ECB)
 for i in (11, 17, 23, 29):
     pass_ECB[i] = crypted_string[k]
     k += 1
-print(binascii.hexlify(bytearray(p)))
+print(binascii.hexlify(bytearray(initial_ECB)))
 print(binascii.hexlify(bytearray(pass_ECB)))
 # encobj = AES.new(key,mode,iv)
